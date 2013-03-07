351f0e4fcc0397a3bd4845afc52ecb44836f478f
Kyle Vermeer <kvermeer@stanford.edu>
2013-03-07 01:17:21 -0800

    Initial Commit

---
+++ HoopStats/HSViewController.h
@@ -0,0 +1,13 @@
+//
+//  HSViewController.h
+//  HoopStats
+//
+//  Created by Kyle Vermeer on 3/7/13.
+//  Copyright (c) 2013 Kyle Vermeer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSViewController : UIViewController
+
+@end
